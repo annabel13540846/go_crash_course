14c9caab3ffa9e84f6139cb11b713b67e34c665d
annabelcowley <13540846@student.uts.edu.au>
2020-09-08 15:05:50 +1000

    Completed structs folder

---
+++ 12_structs/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// Define person struct
+type Person struct {
+	firstName string
+	lastName  string
+	city      string
+	gender    string
+	age       int
+
+	// firstName, lastName, city, gender string
+	// age int
+}
+
+// Greeting method (value reciever)
+func (p Person) greet() string {
+	return "Hello my name is " + p.firstName + " " + p.lastName + " and I am " + strconv.Itoa(p.age)
+}
+
+// hasBirthday method (pointer reciever)
+func (p *Person) hasBirthday() {
+	p.age++
+}
+
+// getMarried
+func (p *Person) getMarried(spouseLastName string) {
+	if p.gender == "m" {
+		return
+	} else {
+		p.lastName = spouseLastName
+	}
+}
+
+func main() {
+	// Init person using struct
+	person1 := Person{firstName: "Samantha", lastName: "Smith", city: "Sydney", gender: "F", age: 20}
+	// Alternative
+	person2 := Person{"Bob", "Williams", "Melbourne", "M", 30}
+
+	// fmt.Println(person1)S
+	// fmt.Println(person1.firstName)
+	// person1.age++
+	// fmt.Println(person1)
+
+	person1.hasBirthday()
+	person1.getMarried("Jones")
+	person2.getMarried("Thompson")
+	fmt.Println(person2.greet())
+
+}
