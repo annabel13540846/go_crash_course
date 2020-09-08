9b8da43677400964d546a8aaa6071743a6a0e239
annabelcowley <13540846@student.uts.edu.au>
2020-09-08 11:36:51 +1000

    Completed variables folder

--- 02_var/main.go
+++ 02_var/main.go
@@ -4,8 +4,16 @@ import "fmt"
 
 func main() {
 	// Using var
-	var name string = "Annabel"
+	//var name = "Annabel"
+	// Shorthand
+	//name := "Brad"
+	var age int32 = 20
+	const isCool = true
+	// size := 1.3
+	// var size float32 = 2.3
+	name, email := "Brad", "brad@gmail.com"
 
-	fmt.Println(name)
+	fmt.Println(name, age, isCool, email)
+	fmt.Printf("%T\n", email)
 
 }
